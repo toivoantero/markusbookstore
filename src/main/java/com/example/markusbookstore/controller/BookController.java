d6d4e3f185d4d086f6156c12e8709abf90d41238
toivoantero <125696622+toivoantero@users.noreply.github.com>
2023-09-21 20:44:41 +0300

    tehtävä 5.1 tehty

--- src/main/java/com/example/markusbookstore/controller/BookController.java
+++ src/main/java/com/example/markusbookstore/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.example.markusbookstore.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.markusbookstore.domain.Book;
 import com.example.markusbookstore.domain.BookRepository;
@@ -48,6 +52,16 @@ public class BookController {
 	    	model.addAttribute("book", repository.findById(bookId) );
 	    	model.addAttribute("categories", crepository.findAll());
 	        return "editbook";
-	    } 
+	    }
+	    
+	    @RequestMapping(value="/books", method = RequestMethod.GET)
+	    public @ResponseBody List<Book> bookListRest() {	
+	        return (List<Book>) repository.findAll();
+	    }
+	    
+	    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	    public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {	
+	    	return repository.findById(bookId);
+	    }
 	  
 }
