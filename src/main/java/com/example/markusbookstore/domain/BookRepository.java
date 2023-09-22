1d8ad54ead71b8a631b72f66acaa2378cc7fc006
toivoantero <125696622+toivoantero@users.noreply.github.com>
2023-09-22 23:28:49 +0300

    bookstore + Spring Data Rest

--- src/main/java/com/example/markusbookstore/domain/BookRepository.java
+++ src/main/java/com/example/markusbookstore/domain/BookRepository.java
@@ -3,8 +3,12 @@ package com.example.markusbookstore.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
-	List<Book> findByTitle(String title);
+	List<Book> findByTitle(@Param("title") String title);
+	List<Book> findByPrice(@Param("price") int price);
     
 }
--- src/main/java/com/example/markusbookstore/domain/CategoryRepository.java
+++ src/main/java/com/example/markusbookstore/domain/CategoryRepository.java
@@ -2,8 +2,11 @@ package com.example.markusbookstore.domain;
 
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-    List<Category> findByName(String name);
+    List<Category> findByName(@Param("name") String name);
     
 }
